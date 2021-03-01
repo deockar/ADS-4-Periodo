d93caccf8dc82d153c94cafbf30ec68bd8df89d4
Deockar <deockar.alexandre2010@hotmail.com>
2021-03-01 20:32:00 -0300

    Atualização com o Swagger

--- cursos/src/main/java/com/deockar/cursos/CursosApplication.java
+++ cursos/src/main/java/com/deockar/cursos/CursosApplication.java
@@ -1,13 +1,61 @@
 package com.deockar.cursos;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@EnableSwagger2
 @SpringBootApplication
 public class CursosApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursosApplication.class, args);
 	}
-
+	@Value("${ads04.swagger.path}")
+	private String swaggerPath;
+	
+	@Bean
+	public Docket allApi() {
+		//Adding Header
+//		ParameterBuilder aParameterBuilder = new ParameterBuilder();
+//		aParameterBuilder.name("Authorization").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+		List<Parameter> aParameters = new ArrayList<Parameter>();
+//		aParameters.add(aParameterBuilder.build());    	
+		Set<String> protocols = new HashSet<>();
+		protocols.add("http");
+		protocols.add("https");
+		return new Docket(DocumentationType.SWAGGER_2).host(swaggerPath)
+				.groupName("All")
+				.apiInfo(apiInfo())
+				.select()
+				.paths(PathSelectors.any())
+				.build()
+				.protocols(protocols)
+				.ignoredParameterTypes(ApiIgnore.class)
+				.enableUrlTemplating(true).globalOperationParameters(aParameters);
+	}
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Curso API")
+				.description("Sistema de Cadastro de ADS04")
+				.termsOfServiceUrl("http://localhost:8080")
+				.license("")
+				.licenseUrl("http//localhost:8080")
+				.version("2.0")
+				.build();
+	}	
 }
